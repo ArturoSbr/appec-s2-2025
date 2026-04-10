e0be86f24a760b5490d2ee4bcc3674e534d471f5
aldomuller <aldomuller75@gmail.com>
2026-04-09 20:40:29 -0600

    My PSM solution

--- assignments/a01/code/sol-153292.py
+++ assignments/a01/code/sol-153292.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.distance import cdist
 
+
+
 # Read data
 df = pd.read_csv('../data/df.csv')
 
@@ -27,9 +29,10 @@ for i in range(dist.shape[1]):
     # checamos si la distancia esta dentro del caliper
     if dist[idx, i] <= THR:
         # sacamos el indice original del tratado en el dataframe
-        idx_1 = df[df['treat'].eq(1)].iloc[i].name.item()
+        idx_1 = df[df['treat'].eq(1)].iloc[i].name
         # sacamos el indice original del control en el dataframe
-        idx_0 = df[df['treat'].eq(0)].iloc[idx].name.item()
+        idx_0 = df[df['treat'].eq(0)].iloc[idx].name
+        
 
         # guardamos el match
         df.loc[idx_1, 'match'] = idx_0
@@ -37,4 +40,6 @@ for i in range(dist.shape[1]):
         # le ponemos inf para que ya no lo puedan agarrar en la siguiente iteracion ya que al poner infito 
         # es la distancia máxima y nada va a superar al infinito entonces por default se quita del pool de reemplazo 
         dist[idx, :] = np.inf
-        
+print(df[['treat', 'match']].head(20))
+print('Matches:', df['match'].notna().sum())
+print('Duplicados:', df['match'].dropna().duplicated().sum())
