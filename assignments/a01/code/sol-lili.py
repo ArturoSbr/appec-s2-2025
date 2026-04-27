1b8d7a9c8fd55f243a2cf1387aaed4f242715560
Liliana Vargas Lugo <lilianavargaslugo@gmail.com>
2026-04-26 23:07:07 -0600

    sol-lili.py

--- assignments/a01/code/sol-lili.py
+++ assignments/a01/code/sol-lili.py
@@ -19,7 +19,18 @@ dist = cdist(p0, p1, metric='euclidean')
 # Iterate through columns (each column i represents the i-th treated worker)
 for i in range(dist.shape[1]):
 
+# Iterate through columns (each column i represents the i-th treated worker)
+for i in range(dist.shape[1]):
     # 1. Find index of the control unit that best matches with treated unit i
+    a = dist[:, i] #me da el vector de distancias del tratado actual contra todos los controles
+    idx = np.argmin(a) #me da la posición del control más cercano dentro del grupo de controles
+    
     # 2. Fetch the index of the best match as it appears in `df`
+    idx_0 = df[df['treat'].eq(0)].iloc[idx].name #identificador en el dataframe por posición y regresa nombre
+
     # 3. Write this index in column `match`
+    idx_1 = df[df['treat'].eq(1)].iloc[i].name #defino idx_1 para encontrar el índice del i tratado
+    df.loc[idx_1, 'match'] = idx_0 
+
     # 4. Remove the matched observation from the pool of candidates
+    dist[idx, :] = 1e6 #elijo distancia grande para que no se vuleva a elegir que se sale de 0 y 1
