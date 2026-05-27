b5e9c1fb70e5fcb5ca6153ad0125dc9e6c3e7cfb
alexis <alexis.capdevielle@itam.mx>
2026-05-26 21:13:28 -0600

    intento de otro path

--- assignments/final/code/main.py
+++ assignments/final/code/main.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 
 # Read dataset
-PATH = './assignments/final/data/wages.csv'
+PATH = os.path.join('..', 'data', 'wages.csv')
 df = pd.read_csv(PATH)
 
 # Strip whitespace from column names (defensive fix)
